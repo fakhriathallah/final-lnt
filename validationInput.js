0ecc19fb5ba97be53eafb03feae57c224c6a3c5a
fakhriathallah <fakhriwongsolo@gmail.com>
2023-05-10 23:35:29 +0700

    first commit

---
+++ validationInput.js
@@ -0,0 +1,59 @@
+const form = document.getElementById("forms");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const email = document.getElementById("email1");
+  const name = document.getElementById("name");
+  const phone = document.getElementById("phone");
+  const dropdown = document.getElementById("dropdown");
+
+  if (!validateEmail(email.value)) {
+    alert("Please enter a valid email address.");
+    email.focus();
+    return false;
+  }
+
+  if (!validateName(name.value)) {
+    alert("Please enter a name with at least 3 characters.");
+    password.focus();
+    return false;
+  }
+
+  if (!validatePhone(phone.value)) {
+    alert("Please enter a phone number with maximum 14-digit phone number.");
+    phone.focus();
+    return false;
+  }
+
+  if (!validateDropdown(dropdown.value)) {
+    alert("Please select an option from the dropdown.");
+    dropdown.focus();
+    return false;
+  }
+
+  // Form is valid, submit it
+  alert("Form submitted successfully!");
+  form.submit();
+});
+
+function validateEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+function validateName(name) {
+  return name.length >= 3;
+}
+
+function validatePhone(phone) {
+  const phoneRegex = /^\d{10}$/;
+  const firstDigit = 1;
+  const secondDigit = 2;
+  phone.charAt(0) == firstDigit && phone;
+  return phoneRegex.test(phone) && phone.length <= 14;
+}
+
+function validateDropdown(dropdown) {
+  return dropdown !== "";
+}
